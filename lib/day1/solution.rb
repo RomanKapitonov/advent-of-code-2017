7b62534e3cdc2c9bb2e792d95d8459175c3afa71
Roman Kapitonov <RomanKapitonov@coherentsolutions.com>
2017-12-20 19:51:29 +0300

    Day1

---
+++ lib/day1/solution.rb
@@ -0,0 +1,41 @@
+class Solver
+  attr_reader :number_list
+
+  def initialize(number:)
+    @number_list = number.scan(/\d/).map(&:to_i)
+  end
+
+  def part1
+    zipped_with_rotated.reduce(0) do |result, (a, b)|
+      result += a if a == b
+      result
+    end
+  end
+
+  def part2
+    zipped_with_twice_rotated.reduce(0) do |result, (a, b)|
+      result += a if a == b
+      result
+    end
+  end
+
+  def rotated_number_list
+    number_list.rotate
+  end
+
+  def twice_rotated_number_list
+    number_list.rotate(half_length)
+  end
+
+  def zipped_with_twice_rotated
+    number_list.zip(twice_rotated_number_list)
+  end
+
+  def zipped_with_rotated
+    number_list.zip(rotated_number_list)
+  end
+
+  def half_length
+    number_list.length / 2
+  end
+end
---
+++ spec/day1/solution_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'day1/solution'
+
+RSpec.describe Solver do
+  shared_examples 'calculates resulting number for part1' do |number:, expected:|
+    context "for number #{number}" do
+      subject(:solver) { described_class.new(number: number) }
+
+      it "resolves to #{expected}" do
+        expect(solver.part1).to eq(expected)
+      end
+    end
+  end
+
+  shared_examples 'calculates resulting number for part2' do |number:, expected:|
+    context "for number #{number}" do
+      subject(:solver) { described_class.new(number: number) }
+
+      it "resolves to #{expected}" do
+        expect(solver.part2).to eq(expected)
+      end
+    end
+  end
+
+  describe '#part1' do
+    {
+      "1122"                      => 3,
+      "1111"                      => 4,
+      "1234"                      => 0,
+      "91212129"                  => 9,
+      File.read('lib/day1/input') => 1029
+    }.each do |number, expected|
+      include_examples 'calculates resulting number for part1', number: number, expected: expected
+    end
+  end
+
+  describe '#part2' do
+    {
+      "1212"                      => 6,
+      "1221"                      => 0,
+      "123425"                    => 4,
+      "123123"                    => 12,
+      "12131415"                  => 4,
+      File.read('lib/day1/input') => 1220
+    }.each do |number, expected|
+      include_examples 'calculates resulting number for part2', number: number, expected: expected
+    end
+  end
+end
